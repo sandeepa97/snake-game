9caa5048f0e856006fec0384e16aa560578d7dbd
sandeepa97 <lbsandeepa@gmail.com>
2021-08-19 23:46:58 +0530

    layout changes

--- index.py
+++ index.py
@@ -13,8 +13,8 @@ green = (0, 255, 0)
 # blue = (50, 153, 213)
 blue = (255, 255, 255)
  
-dis_width = 600
-dis_height = 400
+dis_width = 800
+dis_height = 600
  
 dis = pygame.display.set_mode((dis_width, dis_height))
 pygame.display.set_caption('Sandipz Snake Game')
